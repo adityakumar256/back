df8910db093148f11baf148808eeab12dffc9eb7
Aditya Kumar <aditya.95080@gmail.com>
2025-07-07 01:01:07 +0530

    Fix: correct Hero.jsx import case

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://rgpvpathshala.onrender.com/'
 }));
 
 // ✅ API prefixes — sab perfect
